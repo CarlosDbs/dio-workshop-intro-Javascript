2fc58b461070ef55f492f6313ee7d16329687aa1
Carlos Eduardo Barros Souza <carlos_dbs@hotmail.com>
2019-12-12 12:44:19 -0300

    Introdução Javascript - Digital Innovation One

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+alert("Meu primeiro JS Externo");
+
+var nome = "Fallen";
+var equipe = "M1br";
+var funcao = "AWP";
+var idade = 28;
+var inicioJogo = 12;
+
+alert("Bem Vindo " + nome);
+alert("O player " + nome + " tem " + idade + " anos!");
+alert("O player jogou CS pela primeira vez com " + inicioJogo + " e hoje tem " + idade + " anos!!!");
+
+
+//Somando idades
+alert(idade + inicioJogo);
+
+//Problema ao concaternar e somar
+alert("Somando idades " + Number.parseInt(idade + inicioJogo));
+
+
+//Strings
+/**
+ * Nestes trechos iremos manipular algumas strings
+ */
+var clubeBrasileiro = "Flamengo";
+var clubeIngles = "Liverpool";
+
+ var frase = "O " + clubeBrasileiro + " Ganhará O Mundial De Clubs De 2019";
+
+ console.log(frase);
+
+ console.log(frase.replace(clubeBrasileiro, clubeIngles)); 
+
+ //Alterando o Case das Strings
+ console.log(frase.toUpperCase());
+ console.log(frase.toLowerCase());
